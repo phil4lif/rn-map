921fe683a852984efb6699a68f61c03ef4485811
phil cowan <philipjcowan1@gmail.com>
2020-06-22 12:29:58 -0700

    server setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoUri = 'mongodb://localhost/tracker'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi There!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
