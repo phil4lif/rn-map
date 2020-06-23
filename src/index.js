c975539f8e4ae822379d061296b5b604ce3ad8ea
phil cowan <philipjcowan1@gmail.com>
2020-06-23 11:36:07 -0700

    adds track odel and get all route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-require('./models/User')
+require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb://localhost/tracker'
 mongoose.connect(mongoUri, {
