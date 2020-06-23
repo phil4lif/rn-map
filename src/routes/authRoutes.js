e5fce66271d5b8a8f7a261d35afffe977cd76797
phil cowan <philipjcowan1@gmail.com>
2020-06-22 17:58:42 -0700

    sign in route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,7 +15,25 @@ router.post('/signup' , async (req, res) => {
     } catch (err) {
         return res.status(422).send(err.message);
     }
+});
 
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: 'must provide email and password' })
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(422).send({ error: 'Invalid password or email'});
+    }
+    try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+    res.send({ token });
+    } catch (err) {
+        return res.status(422).send({ error: 'Invalid password or email' });
+    }
 });
 
 module.exports = router;
